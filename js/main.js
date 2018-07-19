45409599462b226f3f4d87f66824d4500f3fe780
Tiago Rodrigues Gomes <tiago.rg@Live.com>
2018-07-19 20:06:53 -0300

    update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+});
+
+$(".rslides_portifolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+});
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".introduction h4").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".about li").addClass("animated flipInX");
+	}, 400);
+	setTimeout(function () {
+		$(".portifolio img").addClass("animated flipInX");
+	}, 400);
+
+});
+
